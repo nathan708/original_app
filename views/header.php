ae432a40500fac77e338692b09fdcc0fc93d1d92
横山直哉 <nathan708@hotmail.co.jp>
2021-06-09 14:03:10 +0900

    headerを少し改良

--- views/header.php
+++ views/header.php
@@ -1,10 +1,9 @@
-
-
-
   <div class="wrapper">
     <header id="page_header">
-    <!-- ログイン前トップ -->
 
+
+    <!-- もしセッションの中身が空ならログイン前トップ (ログインしたらsessionになにか入っているはずだから)-->
+    <?php if (!isset($_SESSION)) { ?>
       <h1><a href="/">サブスクリプション管理</a></h1>
       <nav>
         <ul class="top_nav">
@@ -13,6 +12,22 @@
           <li><a href="/contact">お問い合わせ</a></li>
         </ul>
       </nav>
+
+
+<!-- もしSessionに入っていたらログイン後のトップ -->
+      <?php } elseif (isset($_SESSION)) { ?>
+      <h1><a href="/mypage">サブスクリプション管理</a></h1>
+      <nav>
+        <ul class="top_nav">
+          <li><a href="/mypage/mylist">マイサブスクリプション一覧</a></li>
+          <li><a href="/mypage/mylist/regist">マイサブスクリプション登録</a></li>
+          <li><a href="/contact">お問い合わせ</a></li>
+          <li><a href="/logout">ログアウト</a></li>
+        </ul>
+      </nav>
+      <?php } ?>
+
+
 
     </header> 
   </div>

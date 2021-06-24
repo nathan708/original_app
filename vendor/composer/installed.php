e52758528edceb18afb1de876c6300c716e64d2a
横山直哉 <nathan708@hotmail.co.jp>
2021-06-24 21:27:02 +0900

    no message

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e6742d4e718b00c6645dd3a754ffd353312730bd',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e6742d4e718b00c6645dd3a754ffd353312730bd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
